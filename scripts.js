70629daa6cf014537ada926e41c8374ddf1ae4bd
Roxana Valeria Solernou <119822642+RoxxVS@users.noreply.github.com>
2024-03-08 14:10:31 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,48 @@
+//Variables con los botones
+const botonUno = document.getElementById('botonUno');
+const botonDos = document.getElementById('botonDos');
+//Eventos de los botones
+botonUno.addEventListener('click', codificar);
+botonDos.addEventListener('click', decodificar);
+function codificar() {
+    let texto = document.getElementById('texto').value; //Obtengo el texto ingresado para trabajar con él
+    let textarea = document.getElementById('texto'); //Creo un elemento con el objeto textarea
+    let miTexto; //Defino una variable donde guardar el texto nuevo
+    //Creo el texto nuevo cambiando las letras
+    miTexto = texto.replace(/a/ig, '1').replace(/o/ig, '2').replace(/r/ig, '3').replace(/b/ig, 'a').replace(/c/ig, 'o').replace(/d/ig, 'r').replace(/q/ig, 'b').replace(/u/ig, 'c').replace(/e/ig, 'd').replace(/n/ig, 'q4').replace(/l/ig, 'u5').replace(/s/ig, 'e6').replace(/t/ig, 'n').replace(/m/ig, 'l');
+    textarea.value = miTexto; //Cambio el valor del textarea por el nuevo texto
+    //Modifico los botones
+    botonUno.innerText = 'Copiar';
+    botonUno.removeEventListener('click', codificar);
+    botonUno.addEventListener('click', copiar);
+    botonDos.innerText = 'Borrar';
+    botonDos.removeEventListener('click', decodificar);
+    botonDos.addEventListener('click', borrar);
+}
+function decodificar() {
+    let texto = document.getElementById('texto').value;
+    let textarea = document.getElementById('texto');
+    let miTexto = textarea.value;
+    miTexto = texto.replace(/l/ig, 'm').replace(/n/ig, 't').replace(/e6/ig, 's').replace(/u5/ig, 'l').replace(/q4/ig, 'n').replace(/d/ig, 'e').replace(/c/ig, 'u').replace(/b/ig, 'q').replace(/r/ig, 'd').replace(/o/ig, 'c').replace(/a/ig, 'b').replace(/3/ig, 'r').replace(/2/ig, 'o').replace(/1/ig, 'a');
+    textarea.value = miTexto;
+    botonUno.innerText = 'Copiar';
+    botonUno.removeEventListener('click', codificar);
+    botonUno.addEventListener('click', copiar);
+    botonDos.innerText = 'Borrar';
+    botonDos.removeEventListener('click', decodificar);
+    botonDos.addEventListener('click', borrar);
+}
+function copiar() {
+    let textarea = document.getElementById('texto');
+    navigator.clipboard.writeText(textarea.value);
+}
+function borrar() {
+    let textarea = document.getElementById('texto');
+    textarea.value = '';
+    botonUno.innerText = 'Codificar';
+    botonUno.removeEventListener('click', copiar);
+    botonUno.addEventListener('click', codificar);
+    botonDos.innerText = 'Decodificar';
+    botonDos.removeEventListener('click', borrar);
+    botonDos.addEventListener('click', decodificar);
+}
